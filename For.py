21affab6e861df0c8ad006c3782b827421e4f0a6
gleysondev <gleysonrytholz@gmail.com>
2026-05-11 21:46:55 -0300

    exibiçao de preços

--- For.py
+++ For.py
@@ -1,11 +1,14 @@
 produtos = ["Iphone 16", "Iphone 14", "Iphone 12", "Samsung A24", "Xiaomi Poco x7 Pro"]
 precos = [8000.00, 5000.00, 3000.00, 1300.00, 3000.00]
 quantidades = [5, 7, 5, 9, 4]
+subtotais = []
  
 for indice, produto in enumerate(produtos):
     preco = precos[indice]
     quantidade = quantidades[indice]
-    subtotal = quantidade * preco
+    subtotal = preco * quantidade
+    subtotais.append(subtotal)
+
     
     mensagem = f"""
 -----------------------------------
